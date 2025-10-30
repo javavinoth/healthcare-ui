c86c08159ad23c16b485db13bec5847366afc444
javavinoth <vinoth.sakthivel95@gmail.com>
2025-10-31 00:14:25 +0530

    Updates

--- assets/skeleton-3W01mEq-.js
+++
@@ -1,1 +0,0 @@
-import{j as n,W as r}from"./index-DuMhMhWi.js";function t({className:e,...a}){return n.jsx("div",{className:r("animate-pulse rounded-md bg-neutral-blue-gray/10",e),...a})}export{t as S};
